d5c56f24f48eb7a557ac3c977b59f9cec4b34ac3
Manuel <borsettamanuel96@gmail.com>
2026-01-28 21:44:55 +0100

    modified:   .vscode/c_cpp_properties.json 	new file:   main 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+int main() {
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(300, 450), "Calculator");
 
+    // Set the frame rate limit
+    window.setFramerateLimit(60);
+
+    // Main loop
+    while (window.isOpen()) {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Clear the window with a black color
+        window.clear(sf::Color::Black);
+
+        // Draw things here
+        
+        // Display the contents of the window
+        window.display();
+    }
+
+    return 0;
+}
